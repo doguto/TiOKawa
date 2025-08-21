313fd9917c4de416ea1148b53e479c31e73f2e1d
Minamogu <mina.katsuragi1@gmail.com>
2025-08-21 10:12:56 +0900

    fix class name

--- Assets/TiOKawa/Scenes/Mina_test/Scripts/SoundController.cs
+++ Assets/TiOKawa/Scenes/Mina_test/Scripts/SoundController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SoundPresenter : MonoBehaviour
+public class SoundController : MonoBehaviour
 {
     public AudioClip gatePassClip;
     public AudioClip collisionClip;
