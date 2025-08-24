b0bf41319231744213eac4f892380399b29eedf4
doguto <doguto110@gmail.com>
2025-08-24 14:44:56 +0900

    Feat: マスターデータ関連ファイルの作成

---
+++ Assets/TiOKawa/Scripts/Infra/BinaryGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using MasterMemory;
+using MessagePack;
+using MessagePack.Resolvers;
+using UnityEditor;
+
+namespace TiOKawa.Scripts.Infra
+{
+    public abstract class BinaryGenerator<T>
+    {
+        protected const string BinaryDirectoryPath = "Assets/Project/DataStore/Binary";
+        protected abstract string DataName { get; }
+        protected string BinaryFilePath => Path.Combine(BinaryDirectoryPath, $"{DataName}.bytes");
+        protected string DirectoryName => Path.GetDirectoryName(BinaryFilePath);
+        
+        public abstract void Generate(T data);
+
+        protected DatabaseBuilder CreateMessagePack()
+        {
+            var messagePackResolver = CompositeResolver.Create(
+                MasterMemoryResolver.Instance,
+                StandardResolver.Instance
+            );
+            var options = MessagePackSerializerOptions.Standard.WithResolver(messagePackResolver);
+            MessagePackSerializer.DefaultOptions = options;
+            
+            return new DatabaseBuilder();
+        }
+
+        protected void WriteBinary(DatabaseBuilder builder)
+        {
+            var binaryData = builder.Build();
+            if (!Directory.Exists(DirectoryName)) Directory.CreateDirectory(DirectoryName);
+            
+            File.WriteAllBytes(BinaryFilePath, binaryData);
+            AssetDatabase.Refresh();
+        }
+    }
+}
---
+++ Assets/TiOKawa/Scripts/Infra/MasterData.cs
@@ -0,0 +1,9 @@
+﻿using TiOKawa.Scripts.Infra.Schema;
+
+namespace TiOKawa.Scripts.Infra
+{
+    public class MasterData
+    {
+        public Test[] Tests { get; set; }
+    }
+}
