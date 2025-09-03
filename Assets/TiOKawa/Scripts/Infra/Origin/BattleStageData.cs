0370090b1991e6c572516d7986a3a3992f572c22
doguto <doguto110@gmail.com>
2025-09-04 00:10:38 +0900

    Feat: BattleStageのテーブル作成

---
+++ Assets/TiOKawa/Scripts/Infra/Origin/BattleStageData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using TiOKawa.Scripts.Infra.Schema;
+using UnityEngine;
+
+namespace TiOKawa.Scripts.Infra.Origin
+{
+    [CreateAssetMenu(fileName = "BattleStageData", menuName = "ScriptableObject/BattleStageData")]
+    public class BattleStageData : ScriptableObject
+    {
+        public List<ScriptableBattleStage> battleStages = new();
+    }
+
+    [Serializable]
+    public class ScriptableBattleStage
+    {
+        [SerializeField] int id;
+        [SerializeField] int battleId;
+        [SerializeField] float width;
+        [SerializeField] float spawnPositionZ;
+
+        public BattleStage ToBattleStage()
+        {
+            return new BattleStage()
+            {
+                Id = id,
+                BattleId = battleId,
+                Width = width,
+                SpawnPositionZ = spawnPositionZ
+            };
+        }
+    }
+}
